d200db651e2a25cc2100840475d743341e5ddb8c
Maddytec <maddytec@gmail.com>
2019-10-31 08:47:05 -0300

    Expondo o serviço de consulta de produtos

--- src/main/java/br/com/maddytec/resource/ProdutoResource.java
+++ src/main/java/br/com/maddytec/resource/ProdutoResource.java
@@ -1,7 +1,5 @@
 package br.com.maddytec.resource;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +7,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.maddytec.domain.Produto;
 import br.com.maddytec.dto.ProdutoDTO;
+import br.com.maddytec.model.PageModel;
+import br.com.maddytec.model.PageRequestModel;
 import br.com.maddytec.service.ProdutoService;
 
 @CrossOrigin(origins = "*")
@@ -31,9 +32,14 @@ public class ProdutoResource {
 	}
 	
 	@GetMapping
-	public ResponseEntity<List<Produto>> findAll(){
-		List<Produto> produtos = produtoService.findAll();
-		return ResponseEntity.ok(produtos);
+	public ResponseEntity<PageModel<Produto>> findAllOnLazy(
+			@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "size", defaultValue = "5") int size) {
+
+		PageRequestModel pageRequestModel = new PageRequestModel(page, size);
+
+		PageModel<Produto> pageModel = produtoService.findAllOnLazy(pageRequestModel);
+		return ResponseEntity.ok(pageModel);
 	}
 
 }
--- src/main/java/br/com/maddytec/service/ProdutoService.java
+++ src/main/java/br/com/maddytec/service/ProdutoService.java
@@ -1,11 +1,13 @@
 package br.com.maddytec.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import br.com.maddytec.domain.Produto;
+import br.com.maddytec.model.PageModel;
+import br.com.maddytec.model.PageRequestModel;
 import br.com.maddytec.repository.ProdutoRepository;
 
 @Service
@@ -13,12 +15,15 @@ public class ProdutoService {
 
 	@Autowired
 	ProdutoRepository produtoRepository;
-	
+
 	public Produto save(Produto produto) {
 		return produtoRepository.save(produto);
 	}
-	
-	public List<Produto> findAll(){
-		return produtoRepository.findAll();
+
+	public PageModel<Produto> findAllOnLazy(PageRequestModel pageRequestModel) {
+		PageRequest pageable = PageRequest.of(pageRequestModel.getPage(), pageRequestModel.getSize());
+		Page<Produto> page = produtoRepository.findAll(pageable);
+
+		return new PageModel<>((int) page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
 	}
 }
