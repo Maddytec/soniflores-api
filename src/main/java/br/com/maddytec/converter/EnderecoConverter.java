0d1a71ae99448347e7db36af90f0b62930aff380
Joao Henrique Da Silva Costa <f2118@brasilprev.com.br>
2020-01-03 09:11:25 -0300

    Obtendo stream de entidades diretamente

--- src/main/java/br/com/maddytec/converter/EnderecoConverter.java
+++ src/main/java/br/com/maddytec/converter/EnderecoConverter.java
@@ -1,29 +1,22 @@
 package br.com.maddytec.converter;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 import br.com.maddytec.domain.Cliente;
 import br.com.maddytec.domain.Endereco;
 import br.com.maddytec.dto.ClienteDTO;
-import br.com.maddytec.dto.EnderecoDTO;
 
 @Service
 public class EnderecoConverter {
 
 	public List<Endereco> converterToEnderecosPorClienteDTO(ClienteDTO clienteDTO, Cliente cliente) {
-		Stream<Endereco> streamEntidade =  cliente.getEnderecos().stream();
-
 		return clienteDTO.getEnderecos().stream().map(dto -> {
-			Optional<Endereco> result = streamEntidade.filter(entidade -> entidade.getId() == dto.getId()).findAny();
+			Optional<Endereco> result = cliente.getEnderecos().stream().filter(entidade -> entidade.getId() == dto.getId()).findAny();
 			if (result.isPresent()){
 				BeanUtils.copyProperties(dto, result.get());
 				result.get().setCliente(cliente);
